b0a0f7dbb9b4629f0447380c0d0e0bffcaf0fea6
Tamar Auber <mischegoss@gmail.com>
2019-02-09 12:56:03 -0500

    Lose function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
 
 /*These are the universal variables */
-
+const gems = $(".gems");
 const gem1 =$("#gem1");
 const gem2= $("#gem2");
 const gem3 = $("#gem3");
@@ -13,13 +13,14 @@ const playagain =$("#play-again");
 let win = 0;
 let didWin = false;
 let score = 0;
+let attempts = 0;
 let randomnumber;
 let cardvalueone;
 let cardvaluetwo;
 let cardvaluethree;
 let cardvaluefour;
 
-/* This is the function that sets things up  on load */
+/* This is the function that sets things up  on load.  It is also re-used  when player clicks play again  */
 
 
 function onLoad() {
@@ -29,88 +30,64 @@ function onLoad() {
    setCardValue()
 };
 
+/* This picks a random number and pushes it to the screen  */
 function setRandomNumber() {
   randomnumber =/* Math.floor((Math.random() * 101) + 19); */ 20
   $("#random-number").text(randomnumber)
 }
     
- 
+/*This sets card value on click. It does not print to screen */ 
 function setCardValue()  {
 
-  cardvalue1 = /*Math.floor((Math.random() * 12) + 1); */  5
+  cardvalue1 = /*Math.floor((Math.random() * 12) + 1); */  1
   cardvalue2 =  Math.floor((Math.random() * 12) + 1);
   cardvalue3 =  Math.floor((Math.random() * 12) + 1);
   cardvalue4 =  Math.floor((Math.random() * 12) + 1);
 
-gem1.val(cardvalueone)
-gem2.val(cardvaluetwo)
-console.log(cardvaluetwo)
-gem3.val(cardvaluethree)
-gem4.val(cardvaluefour)
+gem1.val(cardvalue1)
+console.log(cardvalue1)
+gem2.val(cardvalue2)
+console.log(cardvalue2)
+gem3.val(cardvalue3)
+console.log(cardvalue3)
+gem4.val(cardvalue4)
+console.log(cardvalue4)
 }
     
-
+gems.click(function() {
+  scoreval.show()
+  score = score + parseInt($(this).val()); 
+  attempts ++
+  scoreval.text(score)
+  console.log(score)
+  checkWin()
+}
+)
     
 
-    gem1.click(function() {
-        scoreval.show()
-        score = score + cardvalue1;  
-        scoreval.text(score)
-        console.log(score)
-        checkWin()
-      }
-    )
-
-    gem2.click(function() {
-        scoreval.show()
-        score = score + cardvalue2;      
-        scoreval.text(score)
-        console.log(score)
-        
-      }
-    )
-    
-    gem3.click(function() {
-      scoreval.show()
-        score = score + cardvalue3;    
-        scoreval.text(score)
-        console.log(score)
-      }
-    )
-
-    gem4.click(function() {
-       scoreval.show()
-        score = score + cardvalue4;    
-        scoreval.text(score)
-        console.log(score)
-      }
-    )
-
+    /* This checks win  */
    function checkWin() {
-    if (randomnumber === score) {
+    if (randomnumber === score  && attempts < 10) {
         win ++
         wintext.text(win)
         scoreval.text("🎉 🎉 ")
         playagain.show()
         $("#win1").attr("src","assets/images/gemblack.jpg");
-        
-
+    } else if (score > randomnumber || attempts === 10) {
+      console.log("lost")
+      scoreval.text("🤯")
     }
-
+   else {
+      console.log("Still Playing")
+    }
    }
 
-   function getCardValues() {
-    let randomnumber =/* Math.floor((Math.random() * 101) + 19); */ 20
-    let cardvalueone = /*Math.floor((Math.random() * 12) + 1); */  5
-    let cardvaluetwo =  Math.floor((Math.random() * 12) + 1);
-    let cardvaluethree =  Math.floor((Math.random() * 12) + 1);
-    let cardvaluefour =  Math.floor((Math.random() * 12) + 1);
-   }
+  /* This sets action when player clicks play again.  The score should reset but win-lose tallies should stay the same */
 
    playagain.click(function() {
     onLoad()
     score = 0;
-    console.log(cardvaluefour,cardvaluethree)
+    console.log(cardvalue4,cardvalue3)
      
    }
  )
